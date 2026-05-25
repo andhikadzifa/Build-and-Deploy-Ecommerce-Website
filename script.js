317eaa522c68ae269551fcc129df81db3c6b4d30
andhikadzifa <andhikadzifa@gmail.com>
2026-05-26 03:16:55 +0700

    Page Build

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Script for navigation bar
+
+var navBar = document.getElementById("navbar");
+var menuBtn = document.getElementById("bar");
+var closeBtn = document.getElementById("close");
+
+if (menuBtn) {
+    menuBtn.addEventListener("click", function () {
+        navBar.classList.add("active");
+    });
+}
+
+if (closeBtn) {
+    closeBtn.addEventListener("click", function () {
+        navBar.classList.remove("active");
+    });
+}
